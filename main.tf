ad09b8844a5fe56693fddc2f7645b5958c0ccdc5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:32 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "connection_string" {
+  for_each     = { for k, v in var.iothub_endpoint_servicebus_topics : k => v if v.connection_string_key_vault_id != null && v.connection_string_key_vault_secret_name != null }
+  name         = each.value.connection_string_key_vault_secret_name
+  key_vault_id = each.value.connection_string_key_vault_id
+}
 resource "azurerm_iothub_endpoint_servicebus_topic" "iothub_endpoint_servicebus_topics" {
   for_each = var.iothub_endpoint_servicebus_topics
 
@@ -5,7 +10,7 @@ resource "azurerm_iothub_endpoint_servicebus_topic" "iothub_endpoint_servicebus_
   name                = each.value.name
   resource_group_name = each.value.resource_group_name
   authentication_type = each.value.authentication_type
-  connection_string   = each.value.connection_string
+  connection_string   = each.value.connection_string != null ? each.value.connection_string : try(data.azurerm_key_vault_secret.connection_string[each.key].value, null)
   endpoint_uri        = each.value.endpoint_uri
   entity_path         = each.value.entity_path
   identity_id         = each.value.identity_id
--- variables.tf
+++ variables.tf
@@ -8,6 +8,8 @@ Required:
 Optional:
     - authentication_type
     - connection_string
+    - connection_string_key_vault_id (alternative to connection_string - read from Key Vault instead)
+    - connection_string_key_vault_secret_name (alternative to connection_string - read from Key Vault instead)
     - endpoint_uri
     - entity_path
     - identity_id
@@ -15,15 +17,17 @@ Optional:
 EOT
 
   type = map(object({
-    iothub_id           = string
-    name                = string
-    resource_group_name = string
-    authentication_type = optional(string) # Default: "keyBased"
-    connection_string   = optional(string)
-    endpoint_uri        = optional(string)
-    entity_path         = optional(string)
-    identity_id         = optional(string)
-    subscription_id     = optional(string)
+    iothub_id                               = string
+    name                                    = string
+    resource_group_name                     = string
+    authentication_type                     = optional(string) # Default: "keyBased"
+    connection_string                       = optional(string)
+    connection_string_key_vault_id          = optional(string)
+    connection_string_key_vault_secret_name = optional(string)
+    endpoint_uri                            = optional(string)
+    entity_path                             = optional(string)
+    identity_id                             = optional(string)
+    subscription_id                         = optional(string)
   }))
 }
 
